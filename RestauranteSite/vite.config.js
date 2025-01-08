cacc29613ea8aa1090f81a52ed7ded77c5a51b0c
Enzo Alves <80132168+Vlade908@users.noreply.github.com>
2025-01-08 16:18:59 -0300

    teste2

--- RestauranteSite/vite.config.js
+++ RestauranteSite/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "C:\Users\vlade\OneDrive\Documentos\GitHub\RestauranteReact\RestauranteSite"
+  base: "./RestauranteReact/RestauranteSite/",
 })
